f92a557760e6ad71263affe94c40cca396afe9d3
joão pedro corinto <you@example.com>
2024-11-08 20:01:30 -0300

    commit java

---
+++ atividades/Conversão_de_cambio1.java
@@ -0,0 +1,7 @@
+function conversaoCambial() {
+    const dolar = 5.50;
+    const reais = parseFloat(prompt("Digite a quantidade de Reais que deseja converter para Dólar: "));
+    const valorConvertido = reais / dolar;
+    console.log(`O valor em Dólares é: $${valorConvertido.toFixed(2)}`);
+}
+conversaoCambial();
