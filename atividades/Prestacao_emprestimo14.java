a0a59a017b26780609222bfb18190ef58a5088d8
PCjoao <joaopedrocorinto2@gmail.com>
2024-11-08 20:28:27 -0300

    Update Prestacao_emprestimo14.java

---
+++ atividades/Prestacao_emprestimo14.java
@@ -0,0 +1,4 @@
+renda_mensal = float(input("Digite a renda mensal do solicitante: "))
+valor_emprestimo = float(input("Digite o valor total do empréstimo solicitado: "))
+num_prestacoes = int(input("Digite o número de prestações desejadas: "))
+valor_maximo_emprestimo = renda_m
