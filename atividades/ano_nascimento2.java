e72bcff16e1327df5fd35f7158c8abfaa8189feb
PCjoao <joaopedrocorinto2@gmail.com>
2024-11-08 20:29:17 -0300

    Update ano_nascimento2.java

---
+++ atividades/ano_nascimento2.java
@@ -0,0 +1,6 @@
+ano_nascimento = int(input("Digite o ano de nascimento: "))
+idade = int(input("Digite a sua idade: "))
+print(f"A idade da pessoa é: {idade} anos.")
+if idade >= 18:
+    nome = input("Digite o seu nome: ")
+    print(f"{nome}, sua entrada foi permitida.")
