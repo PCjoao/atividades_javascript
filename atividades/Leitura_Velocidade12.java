73598f202e8aadfc579052ae98a514f672b2aa16
PCjoao <joaopedrocorinto2@gmail.com>
2024-11-08 20:26:50 -0300

    Update Leitura_Velocidade12.java

---
+++ atividades/Leitura_Velocidade12.java
@@ -0,0 +1,3 @@
+const velocidadeMs = parseFloat(prompt("Digite a velocidade em m/s: "));
+const velocidadeKmh = velocidadeMs * 3.6;
+console.log(A velocidade em km/h é: ${velocidadeKmh.toFixed(2)});
